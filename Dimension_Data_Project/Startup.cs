6e1c92c49f0a1a62114d4e7bf7a2da26077b3240
Girtie Lehoko <glehoko2@hotmail.com>
2020-11-20 11:00:44 +0200

    Modified

--- Dimension_Data_Project/Startup.cs
+++ Dimension_Data_Project/Startup.cs
@@ -33,6 +33,7 @@ namespace Dimension_Data_Project
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddDbContext<DimensionDataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages();
         }
 
